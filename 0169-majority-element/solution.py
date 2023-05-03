fc88fc07bcbd6c2a7b0a168c9ba3f494abce5246
Anfinsen Joseph <85824381+Anfinsen55@users.noreply.github.com>
2023-05-03 17:42:43 +0000

    LeetCode submission Runtime - 175 ms (5.15%), Memory - 17.8 MB (83.46%)

---
+++ 0169-majority-element/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        res, count = nums[0],1
+        for i in range(1, len(nums)):
+            if nums[i] == res: count += 1
+            else:
+                if count == 0: res,count = nums[i],1
+                count -= 1
+        return res
