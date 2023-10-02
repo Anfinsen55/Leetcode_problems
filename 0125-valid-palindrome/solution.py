d5020049aadc3bff87fa845d62ddcafb13ffd6d7
Anfinsen Joseph <85824381+Anfinsen55@users.noreply.github.com>
2023-10-02 17:39:38 +0000

    LeetCode submission Runtime - 57 ms (27.74%), Memory - 17.4 MB (35.32%)

--- 0125-valid-palindrome/solution.py
+++ 0125-valid-palindrome/solution.py
@@ -2,18 +2,13 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         s=s.lower()
         s=''.join(filter(str.isalnum, s))
-                
-        s=list(s)
-        rev1="".join(s)
-        s.reverse()
-        a="".join(s)
-                
-                
+        left=0
+        right=len(s)-1
+        rev1=""
+        for left in range(len(s)):
+            rev1=rev1+s[right-left]
 
-        if a==rev1:
+        if s==rev1:
             return(True)
-        elif a=="":
-            return(True)
-
         else:
             return(False)
