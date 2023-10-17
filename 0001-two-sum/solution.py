4e3dfbe2213a102ee01a1e45a851fe32625598bd
Anfinsen Joseph <85824381+Anfinsen55@users.noreply.github.com>
2023-10-17 14:36:33 +0000

    LeetCode submission Runtime - 58 ms (89.12%), Memory - 17.5 MB (55.59%)

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -1,8 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if ( i!= j and nums[i] + nums[j] == target):
-                    return(i,j)
-        return()
+        premap={}
 
+        for i,n in enumerate(nums):
+            diff=target-n
+            if diff in premap:
+                return [premap[diff],i]
+            premap[n] = i
+        return
+
+     
